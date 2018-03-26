72138046212dcfe34497e49fc99cc2567e35d360
vit134 <vit134@mail.ru>
2018-03-26 23:45:16 +0300

    add hermione conf create setting for build on staging-2 stand

---
+++ .hermione.conf.js
@@ -0,0 +1,16 @@
+module.exports = {
+	baseUrl: 'http://localhost:3000',
+	gridUrl: 'http://0.0.0.0:4444/wd/hub',
+	sets: {
+		desktop: {
+			files: 'hermione/',
+		}
+	},
+	browsers: {
+		chrome: {
+			desiredCapabities: {
+				browserName: 'chrome'
+			}
+		}
+	}
+};
